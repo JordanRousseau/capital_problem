c31d0a6a76744a372f3652dc43626277c23f38c3
TheoLevalet <theo.levalet@gmail.com>
2021-01-26 00:03:01 +0100

    feat: SoC

--- src/capital_problem/core.py
+++ src/capital_problem/core.py
@@ -2,75 +2,19 @@ import pandas
 from decouple import config
 import numpy
 import dashboard
+import summary
 
 
-def column_summary(
-    dataframe: pandas.DataFrame, print_: bool = True, columns_meaning: str = "Month"
-):
-    """Get Mean, Standard Deviation, Minimum and Maximum for dataframe's columns
+def get_reference_spreadsheets(print_: bool = False):
+    """Get the reference spreadsheets
+
     Args:
-        dataframe (pandas.DataFrame): dataframe of columns (NaN values are not a problem)
-        print_ (bool, optional): Print summary infos on console. Defaults to True.
-        columns_meaning (str, optional): Meaning for all the columns, it will appears on the summary headers. Defaults to "Month".
+        print_ (bool, optional): print param to print the dataframe. Defaults to True.
 
     Returns:
-        dict: contains headers and data `{"summary_headers": list(str), "summary_data": list(list(str, float, float, float, float))}`
+        pandas.DataFrame: reference spreadsheets
     """
-
-    # Convert the dataframe to beatiful numpy array
-    numpy_array = dataframe.to_numpy().astype(float)
-
-    # Extract headers
-    dataframe_headers = list(dataframe.columns.values)
-
-    # Extract mean
-    means = numpy.nanmean(numpy_array, axis=0)
-
-    # Extract standard deviation
-    stds = numpy.nanstd(numpy_array, axis=0)
-
-    # Initiate summary data and headers lists
-    summary = []
-    summary_headers = [
-        columns_meaning,
-        "Mean",
-        "Standard Deviation",
-        "Minimum",
-        "Maximum",
-    ]
-
-    if print_:
-        print(columns_meaning, "Summary:\n")
-
-    # Itterate on columns
-    for index, header in enumerate(dataframe_headers, start=0):
-
-        # Extract minimum
-        current_min = numpy.nanmin(numpy_array[:, index])
-
-        # Extract maximum
-        current_max = numpy.nanmax(numpy_array[:, index])
-        summary.append([header, means[index], stds[index], current_min, current_max])
-
-        # Print infos
-        if print_:
-            print(
-                header,
-                ": { mean :",
-                str(means[index]),
-                ", std :",
-                str(stds[index]),
-                ", min :",
-                str(current_min),
-                ", max :",
-                str(current_max),
-                "}",
-            )
-
-    return {"summary_headers": summary_headers, "summary_data": summary}
-
-
-def run():
+    # Import XLSX
     reference_spreadsheets = pandas.read_excel(
         io=config("CLIMATE_PATH"),
         sheet_name=config("CLIMATE_SHEET_SI"),
@@ -78,7 +22,7 @@ def run():
         usecols=config("CLIMATE_COL_RANGE"),
     )
 
-    # select rows wich day on column 0 starting with 'Jn' where n is a number
+    # Select rows wich day on column 0 starting with 'Jn' where n is a number
     reference_spreadsheets = reference_spreadsheets[
         reference_spreadsheets.iloc[:, int(config("DAY_COL_INDEX"))].str.contains(
             "^J[0-9]+$"
@@ -86,13 +30,23 @@ def run():
         == True
     ]
 
-    print(reference_spreadsheets)
+    if print_:
+        print(reference_spreadsheets)
+
+    return reference_spreadsheets
+
+
+def run(debug: bool = bool(int(config("DEBUG")))):
+    # Get reference spreadsheets
+    reference_spreadsheets = get_reference_spreadsheets(print_=debug)
 
     # Make mean of month columns
-    month_summary = column_summary(
+    month_summary = summary.column_summary(
         dataframe=reference_spreadsheets.iloc[
             :, list(map(int, str(config("MONTH_COLUMNS")).split(",")))
-        ]
+        ],
+        print_=debug,
+        columns_meaning="Months",
     )
 
     dashboard.build_app_report(
@@ -103,7 +57,7 @@ def run():
                 id="summary-table",
             ),
         ]
-    ).run_server(debug=True)
+    ).run_server(debug=debug)
 
 
 if __name__ == "__main__":
