79ace586cfa2fa5dcea9688989b17e1617d5b8d5
TheoLevalet <theo.levalet@gmail.com>
2021-01-31 23:12:30 +0100

    feat: dtw + df instead of area under

--- src/capital_problem/compute.py
+++ src/capital_problem/compute.py
@@ -1,14 +1,15 @@
 import pandas
 import numpy
+import similaritymeasures
 
 
-def area_between(
+def stats_between_series(
     xaxis_1: pandas.Series,
     values_1: pandas.Series,
     xaxis_2: pandas.Series,
     values_2: pandas.Series,
-) -> float:
-    """Compute area between two ordered dataframes
+) -> dict:
+    """Dynamic time warping and discret frechet distance for measuring similarity between two temporal sequences
 
     Args:
         xaxis_1 (pandas.Series): index axis of the dataframe 1
@@ -17,7 +18,7 @@ def area_between(
         values_2 (pandas.Series): value axis of the dataframe 2
 
     Returns:
-        float: Area between curves
+        dict: `{"dtw": float, "frechet_dist": float}`
     """
 
     dataframe_1 = pandas.merge(xaxis_1, values_1, right_index=True, left_index=True)
@@ -33,8 +34,6 @@ def area_between(
     dataframe_1.set_index("id", inplace=True)
     dataframe_2.set_index("id", inplace=True)
 
-    print(dataframe_2, dataframe_1)
-
     unified = pandas.concat([dataframe_1, dataframe_2], axis=1)
     unified["values_1"] = pandas.to_numeric(
         unified["values_1"], errors="coerce", downcast="float"
@@ -43,23 +42,13 @@ def area_between(
         unified["values_2"], errors="coerce", downcast="float"
     ).interpolate()
 
-    # let us generate fake test data
     xaxis_arranged = numpy.arange(len(unified))
-    dataframe_values_1[:, 1] = xaxis_arranged
-    dataframe_values_2[:, 1] = xaxis_arranged
-    dataframe_values_1[:, 1] = unified["values_1"].values
-    dataframe_values_2[:, 1] = unified["values_2"].values
-    y2 = unified["values_2"].values
+    dataframe_values_2 = numpy.array([xaxis_arranged, unified["values_2"].values])
+    dataframe_values_1 = numpy.array([xaxis_arranged, unified["values_1"].values])
 
-    return similaritymeasures.area_between_two_curves(
+    dtw, d = similaritymeasures.dtw(dataframe_values_1, dataframe_values_2)
+    frechet_dist = similaritymeasures.frechet_dist(
         dataframe_values_1, dataframe_values_2
     )
 
-
-if __name__ == "__main__":
-    area_between(
-        xaxis_1=pandas.Series(["a", "b", "c", "e"], name="id"),
-        values_1=pandas.Series(["1", "2", "4", "1"], name="values"),
-        xaxis_2=pandas.Series(["a", "b", "d", "e"], name="id2"),
-        values_2=pandas.Series(["1", "7", "7", "1"], name="values2"),
-    )
+    return {"dtw": dtw, "frechet_dist": frechet_dist}
